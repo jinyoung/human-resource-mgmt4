24994ce7efda165d08a1eedb2a15ab4756b0209d
Jinyoung Jang <jyjang@uengine.org>
2022-11-28 10:50:06 +0900

    commit

--- vacation/src/main/java/human/resource/mgmt/event/VacationDaysIntializedEvent.java
+++ vacation/src/main/java/human/resource/mgmt/event/VacationDaysIntializedEvent.java
@@ -1,14 +1,13 @@
 package human.resource.mgmt.event;
 
+
+
+
 import lombok.Data;
 import lombok.ToString;
 
-
-
-
-
-@Data
 @ToString
+@Data
 public class VacationDaysIntializedEvent {
 
     private Long id;
